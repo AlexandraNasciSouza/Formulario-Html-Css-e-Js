dc957a47180a18c31d6c62fb396f983a26e09d54
Alexandra Nascimento <92178712+AlexandraNasciSouza@users.noreply.github.com>
2021-10-08 17:33:19 -0300

    Site em HTML

---
+++ js/efeito.js
@@ -0,0 +1,24 @@
+//var resposta = confirm("Deseja excluir?");
+/*
+console.log(resposta);
+*/
+
+const a = 10;
+const b = 33;
+
+function somar(a, b){
+  return a + b; 
+}
+
+//console.log(somar(2, 5));
+
+document.querySelector("#calcular").addEventListener("click",function (){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+
+    alert(parseInt(valorA)+ parseInt(valorB));
+});
+
+
+
+
